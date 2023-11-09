379df917c3b9153e07cebf3d8adb791b87337b2a
Justin Yoo <justin.yoojh@gmail.com>
2023-11-09 10:25:57 +0900

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# aspire-youtube-summariser
-This provides sample Aspire apps that summarise a YouTube video transcript to a given language
+# Aspire Youtube Summariser
+
+This provides sample Aspire apps that summarise a YouTube video transcript to a given language.
+
+## Prerequisites
+
+- [.NET 8 SDK](https://dotnet.microsoft.com/download/dotnet/8.0?WT.mc_id=dotnet-96932-juyoo)
+- [Visual Studio](https://visualstudio.microsoft.com/vs?WT.mc_id=dotnet-96932-juyoo)
+- [Azure Subscription](https://azure.microsoft.com/free?WT.mc_id=dotnet-96932-juyoo)
+- [Azure OpenAI Service](https://learn.microsoft.com/azure/ai-services/openai/overview?WT.mc_id=dotnet-96932-juyoo)
+
+## Known Issues
+
+- If your YouTube video is long enough, you might exceed the maximum number of tokens that Azure OpenAI Service can handle. For more information, see [Model summary](https://learn.microsoft.com/azure/ai-services/openai/concepts/models?WT.mc_id=dotnet-96932-juyoo#model-summary-table-and-region-availability).
+  - `gpt-35-turbo`: 4K
+  - `gpt-35-turbo-16k`: 16K
+  - `gpt-4`: 4K
+  - `gpt-4-32K`: 32K
+
+## Getting Started
+
+TBD
+
+## Resources
+
+- [What is Aspire?](https://learn.microsoft.com/dotnet-internal/aspire/get-started/aspire-overview?WT.mc_id=dotnet-96932-juyoo)
---
+++ src/YouTubeSummariser.WebApp/Services/YouTubeService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace YouTubeSummariser.WebApp.Services
+{
+    public interface IYouTubeService
+    {
+        Task<string> GetVideoSummaryAsync(string youTubeVideoUrl, string videoLanguageCode = "en", string summaryLanguageCode = "en");
+    }
+
+    public class YouTubeService : IYouTubeService
+    {
+        private readonly HttpClient _http;
+
+        public YouTubeService(HttpClient http)
+        {
+            this._http = http ?? throw new ArgumentNullException(nameof(http));
+        }
+
+        public async Task<string> GetVideoSummaryAsync(string youTubeVideoUrl, string videoLanguageCode = "en", string summaryLanguageCode = "en")
+        {
+            //await Task.Delay(10000);
+            //return "This is a summary of the video";
+
+            var content = new StringContent(JsonSerializer.Serialize(new
+            {
+                videoUrl = youTubeVideoUrl,
+                videoLanguageCode = videoLanguageCode,
+                summaryLanguageCode = summaryLanguageCode
+            }), Encoding.UTF8, "application/json");
+
+            var response = await this._http.PostAsync("summary", content).ConfigureAwait(false);
+            var summary = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return summary;
+        }
+    }
+}
