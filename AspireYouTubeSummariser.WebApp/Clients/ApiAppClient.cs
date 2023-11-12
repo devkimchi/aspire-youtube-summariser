7ef56579a19b7ff698d784a96a1d126085bd1a88
Justin Yoo <justin.yoojh@gmail.com>
2023-11-12 11:59:31 +0900

    Add UI component

--- AspireYouTubeSummariser.WebApp/Clients/ApiAppClient.cs
+++ AspireYouTubeSummariser.WebApp/Clients/ApiAppClient.cs
@@ -5,6 +5,7 @@ namespace AspireYouTubeSummariser.WebApp.Clients;
 public interface IApiAppClient
 {
     Task<List<WeatherForecast>> GetWeatherForecastsAsync();
+    Task<string> GetVideoSummaryAsync(string videoUrl, string videoLanguageCode = "en", string summaryLanguageCode = "en");
 }
 
 public class ApiAppClient : IApiAppClient
@@ -15,9 +16,20 @@ public class ApiAppClient : IApiAppClient
         this._http = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
     }
 
-    public Task<List<WeatherForecast>> GetWeatherForecastsAsync()
+    public async Task<List<WeatherForecast>> GetWeatherForecastsAsync()
     {
-        var result = this._http.GetFromJsonAsync<List<WeatherForecast>>("WeatherForecast");
+        var result = await this._http.GetFromJsonAsync<List<WeatherForecast>>("WeatherForecast")
+                                     .ConfigureAwait(false);
         return result;
+    }
+
+    public async Task<string> GetVideoSummaryAsync(string videoUrl, string videoLanguageCode = "en", string summaryLanguageCode = "en")
+    {
+        var req = new { videoUrl = videoUrl, videoLanguageCode = videoLanguageCode, summaryLanguageCode = summaryLanguageCode };
+        var result = await this._http.PostAsJsonAsync("Summary", req)
+                                     .ConfigureAwait(false);
+        var summary = await result.Content.ReadAsStringAsync()
+                                  .ConfigureAwait(false);
+        return summary;
     }
 }
